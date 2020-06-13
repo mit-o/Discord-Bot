0746a70186c797eb1c67dc2f3145e707c5dce4f2
Sebastian Tarczynski <starczynski7@gmail.com>
2020-06-13 20:48:17 +0200

    organize project - add cogs

--- bot.py
+++ bot.py
@@ -1,20 +1,42 @@
-# bot.py
-
-import os
 import discord
+import os
 from discord.ext import commands
 from dotenv import load_dotenv
 
-
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')  # from .env file
+TOKEN = os.getenv('DISCORD_TOKEN')
 client = commands.Bot(command_prefix='!')
 
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        try:
+            client.load_extension(f'cogs.{filename[:-3]}')
+        except Exception as error:
+            print("{} cannot be loaded. [{}]".format(
+                f'cogs.{filename[:-3]}', error))
+
+'''
 # status check
 @client.event
 async def on_ready(ctx):
     print(f'{client.user} has connected to Discord!')
 
-
+'''
 client.run(TOKEN)
---
+++ cogs/Test.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Test(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.client.user} has connected to Discord!')
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+def setup(client):
+    client.add_cog(Test(client))
