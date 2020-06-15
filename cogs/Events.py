c55aa8e6da1c08059ed13f5dd0e6baa4b47fb9e2
Sebastian Tarczynski <starczynski7@gmail.com>
2020-06-16 00:41:49 +0200

    add task loop with changing presence to on_ready

--- cogs/Events.py
+++ cogs/Events.py
@@ -1,21 +1,26 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 
 class Events(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    async def status_task(self):
+        while True:
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name='!help'))
+            await asyncio.sleep(20)
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f'with {len(self.client.users)} users'))
+            await asyncio.sleep(20)
+            await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f'on {len(self.client.guilds)} servers'))
+            await asyncio.sleep(20)
+
     @commands.Cog.listener()
     async def on_ready(self):
         print(
             f'Ready: {self.client.user} | Servers: {len(self.client.guilds)}')
-
-        await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="test"))
-
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send('Pong!')
+        self.client.loop.create_task(self.status_task())
 
 
 def setup(client):
