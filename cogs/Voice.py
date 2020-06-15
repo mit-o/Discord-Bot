b42c86f108b8f8f6adc537b7954b6805dcaffb6a
Sebastian Tarczynski <starczynski7@gmail.com>
2020-06-15 22:32:33 +0200

    add better join, play, stop, volume, remove leave

--- cogs/Voice.py
+++ cogs/Voice.py
@@ -1,6 +1,55 @@
+import asyncio
+import os
 import discord
+import youtube_dl
+
 from discord.ext import commands
-from discord.utils import get
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 class Voice(commands.Cog):
@@ -8,28 +57,44 @@ class Voice(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def join(self, ctx):
-        try:
-            channel = ctx.message.author.voice.channel
-            await channel.connect()
-        except:
-            await ctx.send(f'You have to join the voice channel')
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
 
     @commands.command()
-    async def leave(self, ctx):
-        voice = get(self.client.voice_clients, guild=ctx.guild)
-        if voice:
-            voice.stop()
-        server = ctx.message.guild.voice_client
-        await server.disconnect()
+    async def play(self, ctx, *, url):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.client.loop)
+            ctx.voice_client.play(player, after=lambda e: print(
+                'Player error: %s' % e) if e else None)
+
+        await ctx.send('*Now playing:*  **{}**'.format(player.title))
 
     @commands.command()
-    async def play(self, ctx):
-        pass
+    async def volume(self, ctx, volume: int):
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send("*Changed volume to {}%*".format(volume))
 
     @commands.command()
     async def stop(self, ctx):
-        pass
+        await ctx.voice_client.disconnect()
+
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError(
+                    "Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
 
 
 def setup(client):
